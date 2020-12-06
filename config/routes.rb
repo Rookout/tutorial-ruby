3a2319993f78de0e3082b8c884b26fb3e344bd45
Nir Yarden <niryar@gmail.com>
2020-12-06 17:06:14 +0200

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get '/tasks' => 'tasks#index'
+  get '/tasks/active' => 'tasks#index'
+  get '/tasks/completed' => 'tasks#index'
+  post '/tasks/toggle_complete_all' => 'tasks#toggle_complete_all'
+  post '/tasks/clear_completed' => 'tasks#clear_completed'
+  post '/' => 'tasks#create'
+  put '/tasks/:id' => 'tasks#update'
+  delete '/tasks/:id' => 'tasks#destroy'
+  root 'tasks#index'
+
+  resources :tasks
+end
